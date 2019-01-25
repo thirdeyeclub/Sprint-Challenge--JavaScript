e8755cabbe4ace1ec37c1015b0b97cea30120e7d
thirdeyeclub <wwwaaavvveeerrr@gmail.com>
2019-01-25 12:13:55 -0500

    made some functions

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,7 +6,10 @@
   * The last parameter accepts a callback 
   * In the body of the function return the callback with the two parameters that you created
 */
-
+const highfunction = 
+(argument1,argument2,cb) =>{
+  return cb(argument1,argument2);
+};
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -14,6 +17,16 @@
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+const sum = (argu1, argu2) => {
+  console.log(argu1 + argu2);
+}
+
+const multi = (argu1, argu2) => {
+  console.log(argu1 * argu2);
+}
+const greeting = (first, last) => {
+  console.log(`Hello ${first} ${last}, nice to meet you!`);
+}
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
 // consume(2,2,add); // 4
